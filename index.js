e881682cf21293aed160d66c4cd6c0b4da0ec9e0
Dave Schumaker <dave@Daves-MacBook-Pro-2.local>
2015-02-13 08:46:36 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+    app = express();
+    http = require('http').Server(app);
+    io = require('socket.io')(http);
+    path = require('path');
+    os = require('os');
+
+// Get local IP address of server for development purposes.
+function getIPaddres() {
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+          var address = interfaces[k][k2];
+          if (address.family === 'IPv4' && !address.internal) {
+              addresses.push(address.address);
+          }
+      }
+  }
+
+  return addresses;
+}
+
+// Display IP of development server in the console.
+// I'm using this so I can easily connect my phone and other devices
+// to the chat app.
+console.log("IP address: " + getIPaddres());
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+  //res.send('<h1>Hello, World!</h1>');
+});
+
+// Store all usernames that are connected to the chat app.
+var usernames = {};
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket){
+  //io.emit('chat message', 'a user has connected. :)');
+  console.log("a user has connected");
+
+  socket.on('username', function(username) {
+    socket.username = username; // we store the username in the socket session for this client
+  })
+
+  socket.on('chat message', function(msg) {
+    //io.emit('username', username);
+    //io.emit('username', socket.username);
+    io.emit('chat message',  socket.username, msg);
+    //console.log('username: ' + socket.username);
+    console.log('(message) ' + socket.username + ": " + msg);
+  });
+
+  socket.on('disconnect', function() {
+    //io.emit('chat message', 'a user has disconnected. :(');
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function() {
+  console.log("Listening on *:3000");
+});
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Dave's Simple Chat Service
+
+I've been playing around with Node.js recently and taking some of the neat online lessons available through [Nodeschool](http://nodeschool.io/). One of the lessons involved streaming realtime data between the client and server using [Socket.io](http://socket.io/).
+
+This sounded insanely powerful and fund, so I took a break from the rest of the lessons and started playing around with Socket.io. They have a tutorial that explains [how to build a simple, realtime chat application](http://socket.io/) using its library in about 20 minutes.
+
+This little web app is more or less a result of tutorial and I've added some additional stuff as I played around with it (e.g., Ask for and display usernames).
